d2481072125ade716131f8ae9cfede91f38dfbb7
KathyAlde21 <katherinealdereteg@gmail.com>
2024-12-05 17:30:31 -0300

    first commit

---
+++ app/src/main/java/cl/bootcamp/ejercicioindividual15/fragment/MatrixFragment.java
@@ -0,0 +1,28 @@
+package cl.bootcamp.ejercicioindividual15.fragment;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Objects;
+
+import cl.bootcamp.ejercicioindividual15.R;
+
+public class MatrixFragment extends Fragment {
+
+    private FragmentMatrixBinding binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentMatrixBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
